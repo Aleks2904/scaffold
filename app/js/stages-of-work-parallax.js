337898ef8368eb0338c53cce8b5478838797dfef
Aleks2904 <Garmatin171@yandex.ru>
2021-05-17 00:04:13 +0700

    parallax

--- app/js/stages-of-work-parallax.js
+++ app/js/stages-of-work-parallax.js
@@ -1,10 +1,32 @@
 $( document ).ready(function() {
-    function parallax(e){
-      this.querySelectorAll('.stages-of-work__parallax-item').forEach(layer => {
-        const speed = layer.getAttribute('data-speed');
-        layer.style.transform = `translate(${e.clientX*speed/1000}px, ${e.clientX*speed/900}px)`;
-      })
+  $.fn.moveIt = function(){
+    var $window = $(window);
+    var instances = [];
+    
+    $(this).each(function(){
+      instances.push(new moveItItem($(this)));
+    });
+    
+    window.onscroll = function(){
+      var scrollTop = $window.scrollTop();
+      instances.forEach(function(inst){
+        inst.update(scrollTop);
+      });
     }
+  }
 
-    document.addEventListener('mousemove', parallax);
+  var moveItItem = function(el){
+    this.el = $(el);
+    this.speed = parseInt(this.el.attr('data-scroll-speed'));
+  };
+
+  moveItItem.prototype.update = function(scrollTop){
+    var pos = scrollTop / this.speed/2;
+
+    this.el.css('transform', 'translate(' + -pos + 'px,' + -pos + 'px)');
+  };
+
+  $(function(){
+    $('[data-scroll-speed]').moveIt();
+  });
 });
